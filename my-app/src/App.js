dbc76e2315a7d4e89a2b22efd95145233eecf3fb
Carlos Eduardo <cpwaldow@gmail.com>
2022-02-06 11:16:17 -0300

    Inicializando o projeto

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Header />
+    </React.StrictMode>
+  );
+}
+
+export default App;
---
+++ my-app/src/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>teste</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
