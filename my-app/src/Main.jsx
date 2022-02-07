59faf20c64381481fccfa4c6dd848d88854ab6cd
Carlos Eduardo <cpwaldow@gmail.com>
2022-02-07 09:30:26 -0300

    Inicializando o componente Main

---
+++ my-app/src/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <h2>main</h2>
+      </main>
+    );
+  }
+}
+
+export default Main;
