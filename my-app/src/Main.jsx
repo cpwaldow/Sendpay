333a78f38f8884d5a7ed999987aae56e51cc3f6a
Carlos Eduardo <cpwaldow@gmail.com>
2022-02-12 00:06:41 -0300

    refatorando o código componentizando o main

--- my-app/src/Main.jsx
+++ my-app/src/Main.jsx
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
 
 import './style/main.css';
-import phone from './img/celphone.svg';
+
+import SendMoney from './SendMoney';
 
 class Main extends Component {
   render() {
     return (
       <main className='container'>
-        <section className='leftContent'>
-          <h2 className='title'>Sending money is Easier than before</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
-            quae.
-          </p>
-
-          <a href='#' className='btn registerMain'>
-            Register
-          </a>
-          <a href='#' className='btn learnMore'>
-            learn more
-          </a>
-        </section>
-        <section>
-          <img src={phone} alt='Celphone' className='phone' />
-        </section>
+        <SendMoney />
       </main>
     );
   }
