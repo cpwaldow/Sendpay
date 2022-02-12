d6f2324b529bfda2aa15727ca3d8d191ea2d9d32
Carlos Eduardo <cpwaldow@gmail.com>
2022-02-12 01:53:56 -0300

    finalizando a parte How it Works

--- my-app/src/HowItWorks.jsx
+++ my-app/src/HowItWorks.jsx
@@ -3,16 +3,46 @@ import React, { Component } from 'react';
 import './style/global.css';
 import './style/main.css';
 
+import carta from './img/carta.svg';
+import flor from './img/flor.svg';
+import carroEntrega from './img/carroEntrega.svg';
+
 class HowItWorks extends Component {
   render() {
     return (
       <React.Fragment>
         <section className='secoundCountent'>
           <h3 className='howItWorks'>How it Works</h3>
-          <p>
+          <p className='howItWorksFirstText'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. At facere
             pariatur maxime consequuntur sint animi.
           </p>
+          <section className='container'>
+            <section>
+              <img src={carta} alt='carta' className='HiwImg' />
+              <h3>In-app transer</h3>
+              <p className='HiwText'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum, deleniti!
+              </p>
+            </section>
+            <section>
+              <img src={flor} alt='flor' className='HiwImg' />
+              <h3>In-app transer</h3>
+              <p className='HiwText'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum, deleniti!
+              </p>
+            </section>
+            <section>
+              <img src={carroEntrega} alt='carro Entrega' className='HiwImg' />
+              <h3>In-app transer</h3>
+              <p className='HiwText'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum, deleniti!
+              </p>
+            </section>
+          </section>
         </section>
       </React.Fragment>
     );
