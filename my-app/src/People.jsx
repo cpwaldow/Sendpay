6833d967b8ad565fc74a7dd51def012b9512cccb
Carlos Eduardo <cpwaldow@gmail.com>
2022-02-14 22:52:24 -0300

    colocando estilo nos comentários

--- my-app/src/People.jsx
+++ my-app/src/People.jsx
@@ -10,30 +10,27 @@ class People extends Component {
         <section className='people'>
           <section>
             <h3>people are saying about us</h3>
-            <p>See all {'>'}</p>
+            <p className='seeAll'>See all {'>'}</p>
           </section>
 
           <section className='sayingAbout'>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              amet veritatis commodi optio dolores laborum ad maiores harum
-              deserunt ex.
+              amet veritatis commodi optio doloresarum
             </p>
           </section>
 
           <section className='sayingAbout'>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              amet veritatis commodi optio dolores laborum ad maiores harum
-              deserunt ex.
+              amet veritatis commodi optio doloresarum
             </p>
           </section>
 
           <section className='sayingAbout'>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              amet veritatis commodi optio dolores laborum ad maiores harum
-              deserunt ex.
+              amet veritatis commodi optio doloresarum
             </p>
           </section>
         </section>
